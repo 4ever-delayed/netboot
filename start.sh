4ca77acf88c718d8db6bb0cde72c71578dfbd1fb
4ever_delayed <razorilich@gmail.com>
2020-11-12 20:28:05 +0300

    some fixes

--- start.sh
+++ start.sh
@@ -22,18 +22,18 @@ for FILE in $WIN_PE_DIR/*.iso
   do
   DEST_DIR=$(basename "${FILE%.*}")
   [ ! -d "${WIN_10_DIR}/$DEST_DIR" ] && mkdir -p "${WIN_10_DIR}/${DEST_DIR}"  || echo "dir for $FILE already exists"
-  sudo mount -o loop  "$FILE"  "${WIN_10_DIR}/$DEST_DIR"  && echo "mounted $FILE"
+  sudo mount -o loop  "$FILE"  "${WIN_10_DIR}/$DEST_DIR" 2>/dev/null && echo "mounted $FILE"
   done
 
 for FILE in $WIN_PE_DIR/*.ISO
   do
   DEST_DIR=$(basename "${FILE%.*}")
   [ ! -d "${WIN_10_DIR}/$DEST_DIR" ] && mkdir -p "${WIN_10_DIR}/${DEST_DIR}"  || echo "dir for $FILE already exists"
-  sudo mount -o loop  "$FILE"  "${WIN_10_DIR}/$DEST_DIR"  && echo "mounted $FILE"
+  sudo mount -o loop  "$FILE"  "${WIN_10_DIR}/$DEST_DIR" 2>/dev/null && echo "mounted $FILE"
   done
 
 
 
 sudo mount -o loop ${WIN_10_ISO_DIR}/*.iso $WIN_10_DIR/win  &&  echo "mounted Win 10 ISO" || echo "already mounted"
-
-sudo docker-compose up -d
+cp ./netbootxyz-config/menus/local/* ./netbootxyz-config/menus/
+docker-compose up -d
--- stop.sh
+++ stop.sh
@@ -1,11 +1,11 @@
 #!/usr/bin/env bash
 WIN_10_DIR=/media/d1/Dist/OS/netbootxyz
 
-sudo docker-compose stop
+docker-compose stop
 
 for DIR in $WIN_10_DIR/*
   do
-  sudo umount "$DIR" && sudo rm -rf "$DIR" && echo "succesfully unmounted $DIR"
+  sudo umount "$DIR" 2>/dev/null && sudo rm -rf "$DIR" && echo "succesfully unmounted $DIR"
   done
 
   sudo losetup -D
