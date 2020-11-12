50eafcc7ae28a9820c68284ea3bb29d5b62f9cad
4ever_delayed <razorilich@gmail.com>
2020-11-13 02:13:25 +0300

    some fixes

--- start.sh
+++ start.sh
@@ -34,5 +34,5 @@ for FILE in ${WIN_PE_DIR}/*.ISO
 
 
 
-sudo mount -o loop ${WIN_10_ISO_DIR}/*.iso ${WIN_10_DIR}/win  &&  echo "mounted Win 10 ISO" || echo "already mounted"
+sudo mount -o loop ${WIN_10_ISO_DIR}/*.iso ${WIN_10_DIR}/win  &&  echo "mounted Win 10 ISO" || echo "error, are you put a windows ISO to ${WIN_10_DIR}/win ?"
 docker-compose up -d --build && cp ./netbootxyz-config/menus/local/* ./netbootxyz-config/menus/
