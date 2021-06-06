3a99093e82fcb5c4cebc1f09e62fcf9231bf36e8
iliya <iliya@debian-buster.ya>
2021-06-06 09:20:18 -0400

    last

--- install.bat
+++ install.bat
@@ -1,4 +1,2 @@
   wpeinit
-  net use Z: \\192.168.201.145\winboot
-  net use Y: \\192.168.201.145\win_serv
-  net use H: \\192.168.201.145\win_serv2012r2
+  net use Y: \\192.168.200.142\winboot
--- start.sh
+++ start.sh
@@ -10,40 +10,38 @@ if [ ! -d "${WIN_10_ISO_DIR}" ]; then
   curl -s https://gist.githubusercontent.com/hongkongkiwi/15a5bf16437315df256c118c163607cb/raw/71cc4bdc43d8c3d8fd2e1aefde710c9e7481d7f7/download-windows-10.sh | bash
 fi
 
-<<<<<<< HEAD
-if [ ! -d "${WIN_PE_DIR}" ]; then
-  mkdir -p ${WIN_PE_DIR} && echo "created WinPE dir"
-=======
-if [ ! -d "$WIN_SERV_2019" ]; then
-  mkdir -p $WIN_SERV_2019 && echo "created Windows_serv_2019 dir"
-fi
+#if [ ! -d "${WIN_PE_DIR}" ]; then
+#  mkdir -p ${WIN_PE_DIR} && echo "created WinPE dir"
+#fi
 
-if [ ! -d "$WIN_SERV_2012" ]; then
-  mkdir -p $WIN_SERV_2012 && echo "created Windows_serv_2012r2 dir"
-fi
+#if [ ! -d "$WIN_SERV_2019" ]; then
+#  mkdir -p $WIN_SERV_2019 && echo "created Windows_serv_2019 dir"
+#fi
+
+#if [ ! -d "$WIN_SERV_2012" ]; then
+#  mkdir -p $WIN_SERV_2012 && echo "created Windows_serv_2012r2 dir"
+#fi
 
 
 
 if [ ! -d "$WIN_PE_DIR" ]; then
   mkdir -p $WIN_PE_DIR && echo "created WinPE dir"
->>>>>>> test
 fi
 if [ ! -f "${WIN_PE_DIR}/HBCD_PE_x64.iso" ]; then
   echo "downloading WinPE ISO..." &&  wget https://www.hirensbootcd.org/files/HBCD_PE_x64.iso -P ${WIN_PE_DIR}
 fi
 
-
 if [ ! -d "${WIN_10_DIR}/win" ]; then
   mkdir -p "${WIN_10_DIR}/win" && echo "created netbootxyz/win dir"
 fi
 
-if [ ! -d "${WIN_10_DIR}/win_serv" ]; then
-  mkdir -p "${WIN_10_DIR}/win_serv" && echo "created netbootxyz/win_serv dir" || echo "failed to create ${WIN_10_DIR}/win_serv dir"
-fi
+#if [ ! -d "${WIN_10_DIR}/win_serv" ]; then
+#  mkdir -p "${WIN_10_DIR}/win_serv" && echo "created netbootxyz/win_serv dir" || echo "failed to create ${WIN_10_DIR}/win_serv dir"
+#fi
 
-if [ ! -d "${WIN_10_DIR}/win_serv2012r2" ]; then
-  mkdir -p "${WIN_10_DIR}/win_serv2012r2" && echo "created netbootxyz/win_serv2012r2 dir" || echo "failed to create ${WIN_10_DIR}/win_serv2012r2 dir"
-fi
+#if [ ! -d "${WIN_10_DIR}/win_serv2012r2" ]; then
+#  mkdir -p "${WIN_10_DIR}/win_serv2012r2" && echo "created netbootxyz/win_serv2012r2 dir" || echo "failed to create ${WIN_10_DIR}/win_serv2012r2 dir"
+#fi
 
 
 
@@ -54,23 +52,18 @@ fi
 #  sudo mount -o loop  "$FILE"  "${WIN_10_DIR}/$DEST_DIR" 2>/dev/null && echo "mounted $FILE"
 #  done
 
-for FILE in  $(ls ${WIN_PE_DIR}/*)
-  do
-  DEST_DIR=$(basename "${FILE%.*}")
-  [ ! -d "${WIN_10_DIR}/$DEST_DIR" ] && mkdir -p "${WIN_10_DIR}/${DEST_DIR}"  || echo "dir for ${FILE} already exists"
-  sudo mount -o loop  "${FILE}"  "${WIN_10_DIR}/${DEST_DIR}"  && echo "mounted $FILE"
-  done
+#for FILE in  $(ls ${WIN_PE_DIR}/*)
+#  do
+#  DEST_DIR=$(basename "${FILE%.*}")
+#  [ ! -d "${WIN_10_DIR}/$DEST_DIR" ] && mkdir -p "${WIN_10_DIR}/${DEST_DIR}"  || echo "dir for ${FILE} already exists"
+#  sudo mount -o loop  "${FILE}"  "${WIN_10_DIR}/${DEST_DIR}"  && echo "mounted $FILE"
+#  done
 
 
 
-<<<<<<< HEAD
-sudo mount -o loop ${WIN_10_ISO_DIR}/* ${WIN_10_DIR}/win  && 
-docker-compose  up -d --build  && cp -r ./custom-menus/* ./netbootxyz-config/menus/local/
-=======
 sudo mount -o loop ${WIN_10_ISO_DIR}/* ${WIN_10_DIR}/win &&
-sudo mount -o loop ${WIN_SERV_2019}/* ${WIN_10_DIR}/win_serv &&
-sudo mount -o loop ${WIN_SERV_2012}/* ${WIN_10_DIR}/win_serv2012r2 &&
-docker-compose up -d --build --force-recreate && cp -r ./custom-menus/* ./netbootxyz-config/menus/local/
->>>>>>> test
-cp ./custom-menus/wimboot ${WIN_10_DIR}/wimboot 
+#sudo mount -o loop ${WIN_SERV_2019}/* ${WIN_10_DIR}/win_serv &&
+#sudo mount -o loop ${WIN_SERV_2012}/* ${WIN_10_DIR}/win_serv2012r2 &&
+docker-compose up -d --build --force-recreate && cp -r ./custom-menus/* ./netbootxyz-config/menus/local/ &&
+cp ./custom-menus/wimboot ${WIN_10_DIR}/wimboot  &&
 cp  winpeshl.ini install.bat ${WIN_10_DIR}/
